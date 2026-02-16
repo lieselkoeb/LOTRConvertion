91df05ddb399463075d731ff9022411fbacff696
Liesel Koeb <lieselmatos@gmail.com>
2026-02-16 13:52:20 -0300

    Lesson 4 - Buttons & properties - Add & modify Button view - Stored property - @State property wrapper - Print statement

--- LOTRConverter/LOTRConverter/ContentView.swift
+++ LOTRConverter/LOTRConverter/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showExchangeInfo = false
+    
     var body: some View {
         ZStack {
             // Background image
@@ -81,9 +83,20 @@ struct ContentView: View {
                Spacer()
                
                // Info button
-               Image(systemName: "info.circle.fill")
-                   .font(.largeTitle)
-                   .foregroundStyle(.white)
+               HStack {
+                   Spacer()
+                   
+                   Button {
+                       showExchangeInfo.toggle()
+                       print("showExchangeInfo value: \(showExchangeInfo)")
+                   } label: {
+                       Image(systemName: "info.circle.fill")
+                           .font(.largeTitle)
+                           .foregroundStyle(.white)
+                   }
+                   .padding(.trailing)
+               }
+               
             }
 //           .border(.blue)
         }
