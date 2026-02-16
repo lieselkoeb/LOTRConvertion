ea44e579398ac64b122eef2f65645ee656c6c138
Liesel Koeb <lieselmatos@gmail.com>
2026-02-16 15:41:43 -0300

    Lesson 7 - Duplicating Our Exchange Rate View - Extract subview - Instance - Use custom view more than once

--- LOTRConverter/LOTRConverter/ExchangeInfo.swift
+++ LOTRConverter/LOTRConverter/ExchangeInfo.swift
@@ -26,69 +26,15 @@ struct ExchangeInfo: View {
                 Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Bellow is a simple guide to our currency exchange rates:")
                     .font(.title3)
                     .padding()
-                // Gold Piece to Gold Penny
-                HStack {
-                    // Left currency image
-                    Image(.goldpiece)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                    // Exchange rate text
-                    Text("1 Gold Piece = 4 Gold Pennies")
-                    // Right currency image
-                    Image(.goldpenny)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                }
                 
-                // Gold Penny to Silver Piece
-                HStack {
-                    // Left currency image
-                    Image(.goldpenny)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                    // Exchange rate text
-                    Text("1 Gold Penny = 4 Silver Pieces")
-                    // Right currency image
-                    Image(.silverpiece)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                }
+                // Exchange rates
+                ExchangeRate(leftImage: .goldpiece, text: "1 Gold Piece = 4 Gold Pennies", rightImage:  .goldpenny)
+
+                ExchangeRate(leftImage: .goldpenny, text: "1 Gold Penny = 4 Silver Pieces", rightImage: .silverpiece)
                 
-                // Silver Piece to Silver Penny
-                HStack {
-                    // Left currency image
-                    Image(.silverpiece)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                    // Exchange rate text
-                    Text("1 Silver Piece = 4 Silver Pennies")
-                    // Right currency image
-                    Image(.silverpenny)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                }
+                ExchangeRate(leftImage: .silverpiece, text: "1 Silver Piece = 4 Silver Pennies", rightImage: .silverpenny)
                 
-                // Silver Penny to Copper Penny
-                HStack {
-                    // Left currency image
-                    Image(.silverpenny)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                    // Exchange rate text
-                    Text("1 Silver Penny = 4 Copper Pennies")
-                    // Right currency image
-                    Image(.copperpenny)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                }
+                ExchangeRate(leftImage: .silverpenny, text: "1 Silver Penny = 100 Copper Pennies", rightImage: .copperpenny)
                 
                 // Done button
                 Button ("Done") {
