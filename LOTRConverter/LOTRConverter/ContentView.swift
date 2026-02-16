5de3ce041d0c5b125848c8d0fd4780483609d0cb
Liesel Koeb <lieselmatos@gmail.com>
2026-02-16 10:51:51 -0300

    Lesson 2 - Laying Out Our Main Screen with Stacks - ZStack - VStack - HStack - Break down screen design - Lay out stacks

--- LOTRConverter/LOTRConverter/ContentView.swift
+++ LOTRConverter/LOTRConverter/ContentView.swift
@@ -9,17 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .padding()
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .foregroundColor(Color.purple)
-            Button("Button") {
+        ZStack {
+            // Background image
+            
+           VStack {
+               // Prancing pony image
+               
+               // Currency exchange text
+               
+               // Conversion section
+               HStack {
+                   // Left conversion section
+                   VStack {
+                       
+                       // Currency
+                       HStack {
+                           // Currency image
+                           
+                           // Currency text
+                       }
+                       
+                       // Text field
+                    }
+                   // Equal Sign
+                   
+                   // Right conversion section
+                   VStack {
+                       
+                       // Currency
+                       HStack {
+                           // Currency text
+                           
+                           // Currency image
+                       }
+                       
+                       // Text field
+                   }
+               }
+               
+               // Info button
             }
         }
-        .padding()
     }
 }
 
