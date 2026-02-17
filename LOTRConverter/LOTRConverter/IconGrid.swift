1339be9ecf83c97cd8e451c54ffdde1e0b539951
Liesel Koeb <lieselmatos@gmail.com>
2026-02-17 09:39:20 -0300

    Lesson 15 - IconGrid - IconGrid View - Add currencies to ContentView - Show Select Currency view

---
+++ LOTRConverter/LOTRConverter/IconGrid.swift
@@ -0,0 +1,37 @@
+//
+//  IconGrid.swift
+//  LOTRConverter
+//
+//  Created by Liesel Matos Koeb on 17/02/26.
+//
+
+import SwiftUI
+
+struct IconGrid: View {
+    @State var currency: Currency
+    var body: some View {
+        LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+            ForEach(Currency.allCases) { currency in
+                if self.currency == currency {
+                    CurrencyIcon(currencyImage: currency.image, currencyName: currency.name)
+                        .shadow(color: .black, radius: 10)
+                        .overlay {
+                            RoundedRectangle (cornerRadius: 25)
+                                .stroke(lineWidth: 3)
+                                .opacity(0.5)
+                        }
+                }
+                else {
+                    CurrencyIcon(currencyImage: currency.image, currencyName: currency.name)
+                        .onTapGesture {
+                            self.currency = currency
+                        }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    IconGrid(currency: .goldPiece)
+}
--- LOTRConverter/LOTRConverter/SelectCurrency.swift
+++ LOTRConverter/LOTRConverter/SelectCurrency.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct SelectCurrency: View {
     @Environment(\.dismiss) var dismiss
-    @State var currency: Currency
+    @State var topCurrency: Currency
+    @State var bottomCurrency: Currency
+    
     var body: some View {
         ZStack {
             // Background
@@ -25,30 +27,14 @@ struct SelectCurrency: View {
                     .fontWeight(.bold)
                 
                 // Icons
-                LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
-                    ForEach(Currency.allCases) { currency in
-                        if self.currency == currency {
-                            CurrencyIcon(currencyImage: currency.image, currencyName: currency.name)
-                                .shadow(color: .black, radius: 10)
-                                .overlay {
-                                    RoundedRectangle (cornerRadius: 25)
-                                        .stroke(lineWidth: 3)
-                                        .opacity(0.5)
-                                }
-                        }
-                        else {
-                            CurrencyIcon(currencyImage: currency.image, currencyName: currency.name)
-                                .onTapGesture {
-                                    self.currency = currency
-                                }
-                        }
-                    }
-                }
+                IconGrid(currency: topCurrency)
                 
                 // Text
                 Text("Select the currency you would like to convert to:")
                     .fontWeight(.bold)
+                    .padding(.top)
                 // Icons
+                IconGrid(currency: bottomCurrency)
                 
                 // Done button
                 Button("Done") {
@@ -63,11 +49,12 @@ struct SelectCurrency: View {
             }
             .padding()
             .multilineTextAlignment(.center)
+            .foregroundStyle(.black)
             
         }
     }
 }
 
 #Preview {
-    SelectCurrency(currency: .silverPiece)
+    SelectCurrency(topCurrency: .silverPiece, bottomCurrency: .copperPenny)
 }
