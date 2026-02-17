e3790ee605ebef392fd54e8c1b6b9003ec870f3e
Liesel Koeb <lieselmatos@gmail.com>
2026-02-17 08:24:50 -0300

    Lesson 11 - Models & Enums - Model - Enum - Raw value

---
+++ LOTRConverter/LOTRConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Liesel Matos Koeb on 17/02/26.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
