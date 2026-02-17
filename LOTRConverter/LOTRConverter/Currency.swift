f668df25689a4fd03aa85c80b479d50f4bde118d
Liesel Koeb <lieselmatos@gmail.com>
2026-02-17 16:51:58 -0300

    Challlenge #1 - Add 6th Currency

--- LOTRConverter/LOTRConverter/Currency.swift
+++ LOTRConverter/LOTRConverter/Currency.swift
@@ -10,6 +10,7 @@ import SwiftUI
 enum Currency: Double, CaseIterable, Identifiable {
     
     case copperPenny = 6400
+    case copperPiece = 1600
     case silverPenny = 64
     case silverPiece = 16
     case goldPenny = 4
@@ -21,6 +22,8 @@ enum Currency: Double, CaseIterable, Identifiable {
         switch self {
         case .copperPenny:
                 .copperpenny
+        case .copperPiece:
+                .copperpiece
         case .silverPenny:
                 .silverpenny
         case .silverPiece:
@@ -36,6 +39,8 @@ enum Currency: Double, CaseIterable, Identifiable {
         switch self {
         case .copperPenny:
             "Copper Penny"
+        case .copperPiece:
+            "Copper Piece"
         case .silverPenny:
             "Silver Penny"
         case .silverPiece:
